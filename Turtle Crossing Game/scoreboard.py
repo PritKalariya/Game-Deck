1665a1e37fe15d95c7ed00de7adcb14189ae2b7e
Prit Kalariya <kalariyaprit2000@gmail.com>
2022-05-13 10:53:39 +0530

    Stored highscore in txt.

--- Turtle Crossing Game/scoreboard.py
+++ Turtle Crossing Game/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.level = 1
+        with open("data.txt", mode="r") as data:
+            self.highestlevel = int(data.read())
         self.hideturtle()
         self.penup()
         self.goto(-280, 250)
@@ -16,11 +18,15 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Level: {self.level}", align="left", font=FONT)
+        self.write(f"Level: {self.level} \tHighest Level: {self.highestlevel}", align="left", font=FONT)
 
 
     def level_up(self):
         self.level += 1
+        if self.level > self.highestlevel:
+            self.highestlevel = self.level
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highestlevel}")
         self.update_scoreboard()
 
 
