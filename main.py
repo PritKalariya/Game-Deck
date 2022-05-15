a28eac965d592ecb276864d8940526b3a4b7e374
Prit Kalariya <kalariyaprit2000@gmail.com>
2022-05-15 17:12:57 +0530

    Created main_page class.

--- main.py
+++ main.py
@@ -3,118 +3,120 @@ from tkinter import messagebox
 import os
 
 
-# Game open functions
-def pong():
-    main.withdraw()
-    if  os.getcwd().split('\\')[-1] == 'Pong Game':
-        os.system("python pong.py")
-    else:
-        os.chdir("Pong Game")
-        os.system("python pong.py")
-        os.chdir("..")
-    main.deiconify()
-
-def snake():
-    main.withdraw()
-    if  os.getcwd().split('\\')[-1] == 'Snake Game':
-        os.system("python main.py")
-    else:
-        os.chdir("Snake Game")
-        os.system("python main.py")
-        os.chdir("..")
-    main.deiconify()
-
-def quiz_game():
-    main.withdraw()
-    if  os.getcwd().split('\\')[-1] == 'Quiz Game (GUI)':
-        os.system("python main.py")
-    else:
-        os.chdir("Quiz Game (GUI)")
-        os.system("python main.py")
-        os.chdir("..")
-    main.deiconify()
-
-def turtle_crossing():
-    main.withdraw()
-    if  os.getcwd().split('\\')[-1] == 'Turtle Crossing Game':
-        os.system("python main.py")
-    else:
-        os.chdir("Turtle Crossing Game")
-        os.system("python main.py")
-        os.chdir("..")
-    main.deiconify()
-
-def hangman():
-    main.withdraw()
-    if  os.getcwd().split('\\')[-1] == 'Hangman Game':
-        os.system("python hangman.py")
-    else:
-        os.chdir("Hangman Game")
-        os.system("python hangman.py")
-        os.chdir("..")
-    main.deiconify()
-
-def profile():
-    main.withdraw()
-    os.system('python profile.py')
-    main.deiconify()
-
-def instructions():
-    main.withdraw()
-    os.system('python instructions.py')
-    main.deiconify()
-
-def exit():
-    dummy = Tk()
-    dummy.destroy()
-    messagebox.showinfo("Exit", "Thank you for playing Game Deck!! \nSee you soon.")
-    main.destroy()
+class main_page():
+    def __init__(self):
+        pass
 
 
-# Main window GUI
-main = Tk()
-main.title("Game Deck")
-main.geometry("1377x768")
-main.resizable(0, 0)
-main.configure(bg="white")
+    def gui(self):
+        # Main window GUI
+        self.main = Tk()
+        self.main.title("Game Deck")
+        self.main.geometry("1377x768")
+        self.main.resizable(0, 0)
+        self.main.configure(bg="white")
 
 
-# bg for tkinter window
-main_bg = Label(main)
-main_bg.place(relx=0, rely=0, width=1377, height=768)
-image = PhotoImage(file="./images/pages/main.png")
-main_bg.configure(image=image)
+        # bg for tkinter window
+        self.main_bg = Label(self.main)
+        self.main_bg.place(relx=0, rely=0, width=1377, height=768)
+        self.image = PhotoImage(file="./images/pages/main.png")
+        self.main_bg.configure(image=self.image)
 
 
-# Game buttons
-# row 1
-snake_game_btn = Button(main, text ="PLAY",bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=snake)
-snake_game_btn.place(x=320, y=352.5)
+        # Game buttons
+        # row 1
+        self.snake_game_btn = Button(self.main, text ="PLAY",bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=self.snake)
+        self.snake_game_btn.place(x=320, y=352.5)
 
-quiz_game_btn = Button(main, text ="PLAY", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=quiz_game)
-quiz_game_btn.place(x=636, y=352.5)
+        self.quiz_game_btn = Button(self.main, text ="PLAY", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=self.quiz_game)
+        self.quiz_game_btn.place(x=636, y=352.5)
 
-pong_game_btn = Button(main, text ="PLAY", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=pong)
-pong_game_btn.place(x=948, y=354)
+        self.pong_game_btn = Button(self.main, text ="PLAY", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=self.pong)
+        self.pong_game_btn.place(x=948, y=354)
 
-# row 2
-turtle_crossing_btn = Button(main, text ="PLAY",bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=turtle_crossing)
-turtle_crossing_btn.place(x=320, y=678)
+        # row 2
+        self.turtle_crossing_btn = Button(self.main, text ="PLAY",bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=self.turtle_crossing)
+        self.turtle_crossing_btn.place(x=320, y=678)
 
-hangman_btn = Button(main, text ="PLAY", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=hangman)
-hangman_btn.place(x=638, y=680)
+        self.hangman_btn = Button(self.main, text ="PLAY", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=self.hangman)
+        self.hangman_btn.place(x=638, y=680)
 
-profile_btn = Button(main, text ="USER", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=profile)
-profile_btn.place(x=953, y=678)
-
-
-# instruction btn
-instruction_btn = Button(main, bg="#0074e4", relief="flat", width=3, height=2, cursor="hand2", command=instructions)
-instruction_btn.place(x=30, y=26)
-
-# exit btn
-exit_btn = Button(main, bg="#0074e4", relief="flat", width=4, height=2, cursor="hand2", command=exit)
-exit_btn.place(x=1312, y=26)
+        self.profile_btn = Button(self.main, text ="USER", bg="#0074e4", relief="flat", width=10, height=1, cursor="hand2", command=self.profile)
+        self.profile_btn.place(x=953, y=678)
 
 
-main.mainloop()
+        # instruction btn
+        self.instruction_btn = Button(self.main, bg="#0074e4", relief="flat", width=3, height=2, cursor="hand2", command=self.instructions)
+        self.instruction_btn.place(x=30, y=26)
+
+        # exit btn
+        self.exit_btn = Button(self.main, bg="#0074e4", relief="flat", width=4, height=2, cursor="hand2", command=self.exit)
+        self.exit_btn.place(x=1312, y=26)
+
+        self.main.protocol("WM_DELETE_WINDOW", self.confirm_exit)
+        self.main.mainloop()
+
+
+
+    # helper methods
+    def run_file(self, name):
+        if os.getcwd().split('\\')[-1] == name:
+            os.system("python main.py")
+        else:
+            os.chdir(name)
+            os.system("python main.py")
+            os.chdir("..")
+
+
+    def pong(self):
+        self.main.withdraw()
+        self.run_file("Pong Game")
+        self.main.deiconify()
+
+    def snake(self):
+        self.main.withdraw()
+        self.run_file("Snake Game")
+        self.main.deiconify()
+
+    def quiz_game(self):
+        self.main.withdraw()
+        self.run_file("Quiz Game (GUI)")
+        self.main.deiconify()
+
+    def turtle_crossing(self):
+        self.main.withdraw()
+        self.run_file("Turtle Crossing Game")
+        self.main.deiconify()
+
+    def hangman(self):
+        self.main.withdraw()
+        self.run_file("Hangman Game")
+        self.main.deiconify()
+
+    def profile(self):
+        self.main.withdraw()
+        os.system('python profile.py')
+        self.main.deiconify()
+
+    def instructions(self):
+        self.main.withdraw()
+        os.system('python instructions.py')
+        self.main.deiconify()
+
+    def exit(self):
+        dummy = Tk()
+        dummy.destroy()
+        messagebox.showinfo("Exit", "Thank you for playing Game Deck!! \nSee you soon.")
+        self.main.destroy()
+
+    def confirm_exit(self):
+        self.dummy = Tk()
+        self.dummy.withdraw()
+        yes_no = messagebox.askyesno("Confirm selection", "Are you sure you want to exit?")
+        if yes_no:
+            self.main.destroy()
+
+
+# demo = main_page()
+# demo.gui()
